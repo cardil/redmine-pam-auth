c3c11f7428621422c0b268357f29911849361808
Krzysztof Suszyński <krzysztof.suszynski@gmail.com>
2012-05-16 21:58:11 +0200

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_pam_auth do
+  name 'Redmine Pam Auth plugin'
+  author 'Krzysztof Suszynski'
+  description 'Adds PAM authorization to Redmine. Requires `rpam` gem and group `shadow` to by added to redmine user.'
+  version '0.1.0'
+  url 'http://github.com/cardil/redmine-pam-auth'
+  author_url 'http://suszynski.org/about'
+end
